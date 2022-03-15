e5264790593de1f3ab6cd3cddc6391c7bc7240e9
administrator <administrator@46557.local>
2022-03-15 11:02:07 -0700

    Add closures example

---
+++ Closures.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+
+import Foundation
+
+func sum(_ a:Int, b:Int) -> Int {
+    return a + b
+    
+}
+
+sum(  2, b: 5)
+
+func calculate(_ a:Int, _ b:Int, operationFunction: (_ a:Int, _ b:Int)-> Int) -> Int {
+   return operationFunction(a, b)
+}
+
+calculate(10, 15, operationFunction: sum)
+
+calculate(23, 15, operationFunction: {(_ a:Int, _ b:Int)-> Int in return a + b})
+
+calculate(14, 15, operationFunction: {a,b in return a + b})
+
+calculate(45, 15, operationFunction: { return $0 + $1})
+
+calculate(7, 15, operationFunction: { $0 + $1})
+
+calculate(9, 15, operationFunction:  +  )
+
+var names = ["bruno","carol","vitor","tais","roberto"]
+
+var result = names.map({$0.uppercased()})
+print(result)
+print(names.map({$0 + " last name"}))
+
+print(names.filter({$0.count < 5}))
+
